fee4d36d035534e382d7357ae14320831adb2130
Will Lin <wlin@localhost.localdomain>
2020-08-31 16:48:59 -0700

    Change PC check to accept optional rng

--- src/sonic_pc/mod.rs
+++ src/sonic_pc/mod.rs
@@ -367,14 +367,14 @@ impl<E: PairingEngine> PolynomialCommitment<E::Fr> for SonicKZG10<E> {
         Ok(proof)
     }
 
-    fn check<'a, R: RngCore>(
+    fn check<'a>(
         vk: &Self::VerifierKey,
         commitments: impl IntoIterator<Item = &'a LabeledCommitment<Self::Commitment>>,
         point: E::Fr,
         values: impl IntoIterator<Item = E::Fr>,
         proof: &Self::Proof,
         opening_challenge: E::Fr,
-        _rng: &mut R,
+        _rng: Option<&mut dyn RngCore>,
     ) -> Result<bool, Self::Error>
     where
         Self::Commitment: 'a,
