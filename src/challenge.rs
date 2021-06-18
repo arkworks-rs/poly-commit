744f77fd08ea4939e5a0d1bedde4f5222cf8e16e
Tom Shen <tomshen@berkeley.edu>
2021-06-17 20:56:56 -0700

    add challenge strategy (more)

--- src/challenge.rs
+++ src/challenge.rs
@@ -1,18 +1,13 @@
-use ark_ff::Field;
+use ark_ff::PrimeField;
 use ark_sponge::FieldBasedCryptographicSponge;
 
-pub enum ChallengeStrategy {
-    Multivariate,
-    Univariate,
-}
-
 /// State stored for univariate generator
-pub struct UnivariateGeneratorState<F: Field> {
+pub struct UnivariateGeneratorState<F: PrimeField> {
     gen: F,
     next: F,
 }
 
-impl<F: Field> UnivariateGeneratorState<F> {
+impl<F: PrimeField> UnivariateGeneratorState<F> {
     fn new(gen: F) -> Self {
         Self { gen, next: gen }
     }
@@ -24,13 +19,16 @@ impl<F: Field> UnivariateGeneratorState<F> {
     }
 }
 
-#[derive(Copy, Clone)]
-pub enum ChallengeGenerator<'a, F: Field, S: 'a + FieldBasedCryptographicSponge<F>> {
+// TODO: Copy and Clone trait cannot be derived
+/// Challenge Generator (todo doc)
+pub enum ChallengeGenerator<'a, F: PrimeField, S: 'a + FieldBasedCryptographicSponge<F>> {
+    /// todo: doc
     Multivariate(&'a mut S),
+    /// todo: doc
     Univariate(UnivariateGeneratorState<F>),
 }
 
-impl<'a, F: Field, S: 'a + FieldBasedCryptographicSponge<F>> ChallengeGenerator<'a, F, S> {
+impl<'a, F: PrimeField, S: 'a + FieldBasedCryptographicSponge<F>> ChallengeGenerator<'a, F, S> {
     /// Returns a challenge generator with multivariate strategy. Each challenge is freshly squeezed
     /// from a sponge.
     pub fn new_multivariate(sponge: &'a mut S) -> Self {
@@ -41,16 +39,19 @@ impl<'a, F: Field, S: 'a + FieldBasedCryptographicSponge<F>> ChallengeGenerator<
     /// squeezed element from sponge.
     pub fn new_univariate(sponge: &mut S) -> Self {
         let gen = sponge.squeeze_native_field_elements(1)[0];
-        let univariate_state = UnivariateGeneratorState { gen, next: gen };
+        let univariate_state = UnivariateGeneratorState::new(gen);
         Self::Univariate(univariate_state)
     }
 
     /// Returns the next challenge generated.
     pub fn next_challenge(&mut self) -> F {
-        if let Self::Multivariate(s) = &mut self {
+        if let Self::Multivariate(s) = self {
             s.squeeze_native_field_elements(1)[0]
-        } else if let Self::Univariate(s) = &mut self {
+        } else if let Self::Univariate(s) = self {
             s.get_next()
+        } else {
+            // should not happen
+            panic!()
         }
     }
 }
