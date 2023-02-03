a8e6b8ad31ef6b8db4905ab048356406a6a6d69e
nikkolasg <nikkolasg@gmail.com>
2023-02-04 00:09:23 +0100

    cargo fmt

--- src/challenge.rs
+++ src/challenge.rs
@@ -1,5 +1,5 @@
-use ark_ff::PrimeField;
 use ark_crypto_primitives::sponge::{CryptographicSponge, FieldElementSize};
+use ark_ff::PrimeField;
 
 /// `ChallengeGenerator` generates opening challenges using multivariate or univariate strategy.
 /// For multivariate strategy, each challenge is freshly squeezed from a sponge.
