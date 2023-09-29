9d9df6081e2562421c4793e74aaf56f716eb7c53
mmagician <marcin.gorny.94@protonmail.com>
2023-09-29 02:21:44 -0600

    Add CODEOWNERS (#124)
    * Add CODEOWNERS
    
    * Update lib.rs
    
    * Update lib.rs
    
    * Update lib.rs
    
    ---------
    
    Co-authored-by: Weikeng Chen <w.k@berkeley.edu>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 //! A crate for polynomial commitment schemes.
 #![deny(unused_import_braces, unused_qualifications, trivial_casts)]
-#![deny(trivial_numeric_casts, private_in_public, variant_size_differences)]
+#![deny(trivial_numeric_casts, variant_size_differences)]
 #![deny(stable_features, unreachable_pub, non_shorthand_field_patterns)]
 #![deny(unused_attributes, unused_mut)]
 #![deny(missing_docs)]
